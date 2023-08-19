f28c2e642ff184e2e9884950eb6d2fa612003293
RedaAwaad <aw25re@gmail.com>
2023-08-19 12:30:47 +0300

    chore(eslint): add vue3-recommended rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,9 +10,15 @@ module.exports = {
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
+    'plugin:vue/vue3-recommended',
     'airbnb-base',
     'plugin:prettier/recommended',
   ],
-  plugins: [],
-  rules: {},
+  plugins: ['vue', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'vue/multi-word-component-names': 'off',
+  },
 };
